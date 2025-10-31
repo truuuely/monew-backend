fb41f3b2358c846770df056d07cefdc82c14431c
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-10-31 11:03:34 +0900

    feat: 배치 기본 설정 및 Naver API 프로퍼티 추가 - batch 모듈 build.gradle 의존성 추가 (monew-api, web, querydsl) - application.yml에 Naver API 설정 (env 기반) 및 Properties 파일 ㅜ가 - EntityScan, EnableJpaRepositories 설정 추가 - QuerydslConfig, RestTemplateConfig 추가 - ArticleSource Enum 정의

--- monew-batch/src/main/java/com/monew/monew_batch/MonewBatchApplication.java
+++ monew-batch/src/main/java/com/monew/monew_batch/MonewBatchApplication.java
@@ -2,9 +2,13 @@ package com.monew.monew_batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.monew.monew_api")
+@EnableJpaRepositories(basePackages = "com.monew.monew_api")
 @EnableScheduling
 public class MonewBatchApplication {
 
