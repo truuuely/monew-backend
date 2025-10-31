b339c60febd2d1e884fc064ddaaf72286f7a78e2
truuuely <kmyc7030@gmail.com>
2025-10-31 09:19:52 +0900

    chore: monew-api와 monew-batch 모듈의 모든 타임존을 UTC 기준으로 변경

--- monew-batch/src/main/java/com/monew/monew_batch/MonewBatchApplication.java
+++ monew-batch/src/main/java/com/monew/monew_batch/MonewBatchApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class MonewBatchApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(MonewBatchApplication.class, args);
 	}
 
