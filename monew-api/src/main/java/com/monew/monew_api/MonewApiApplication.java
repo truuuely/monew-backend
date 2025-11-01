690bdc56f6f349de6586bbf05f30052373c3d462
truuuely <kmyc7030@gmail.com>
2025-11-01 17:51:59 +0900

    fix: 누락된 @EnableAsync 추가 - 이벤트 비동기 처리용

--- monew-api/src/main/java/com/monew/monew_api/MonewApiApplication.java
+++ monew-api/src/main/java/com/monew/monew_api/MonewApiApplication.java
@@ -3,11 +3,13 @@ package com.monew.monew_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class MonewApiApplication {
 
 	public static void main(String[] args) {
